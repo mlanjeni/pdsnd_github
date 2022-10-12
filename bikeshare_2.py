f6ef250b2f6fb9212072006e83b832607e6b6ca2
mlanjeni <mlanjenijoshua@gmail.com>
2022-10-12 20:27:55 +0200

    Refactor: Shorten print statements in .py file

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -40,7 +40,7 @@ def get_filters():
             print('You entered {}'.format(month))
             break
         else:
-            print('\nInvalid month name entered. Please try again')
+            print('\nInvalid month name entered. Try again')
 
     # now user must choose a specific day or all days
     # get user input for day of week (all, monday, tuesday, ... sunday)
@@ -51,7 +51,7 @@ def get_filters():
             print('You entered {}'.format(day))
             break
         else:
-            print('Invalid day name entered. Please try again')
+            print('Invalid day name entered. Try again')
 
     print('-'*40)
     # this returns valid city, month, day inputs of user
@@ -244,7 +244,7 @@ def display_data(df):
             print('You have entered {}'.format(answer))
             print(df.head())
         elif answer not in user_answer_list:
-                print('Invalid input. Please try again')
+                print('Invalid input. Try again')
     # keep asking user to view more 5 raw data 
     # use while loop, meaning as long as the anser is yes, keep asking further
     while answer == 'yes':
